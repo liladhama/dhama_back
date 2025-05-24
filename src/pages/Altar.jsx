5e706bc1be783b51d18d2f4c73a15a6debae499f
liladhama <samadhidas108@gmail.com>
2025-05-24 10:45:56 +0400

    fix: altar layout updated to prevent BottomNav shift

--- src/pages/Altar.jsx
+++ src/pages/Altar.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 export default function Altar() {
   return (
-    <div
-      className="h-full bg-cover bg-center bg-no-repeat"
-      style={{
-        backgroundImage: "url('/images/altar-bg.png')",
-        minHeight: 'calc(100vh - 136px)', // 56 TopBar + 80 BottomNav
-      }}
-    >
-      {/* Здесь можно добавить интерактивные элементы, если нужно */}
+    <div className="relative w-full h-full overflow-hidden flex-grow">
+      {/* Фоновое изображение */}
+      <img
+        src="/images/altar-bg.png"
+        alt="Altar"
+        className="w-full h-full object-cover absolute inset-0 z-0"
+      />
+
+      {/* Контейнер для интерактивных элементов */}
+      <div className="relative z-10 flex flex-col items-center justify-end h-full pb-4">
+        {/* Здесь можно разместить кнопки подношений */}
+      </div>
     </div>
   );
 }
