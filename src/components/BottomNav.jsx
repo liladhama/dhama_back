e1991667b7a6d217b4d5ef71f03c8740ca7cc933
liladhama <samadhidas108@gmail.com>
2025-05-24 11:16:14 +0400

    revert: restore original altar and bottomnav layout

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -9,26 +9,31 @@ import marketIcon from '../icons/icon_market.png';
 
 export default function BottomNav() {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-[#DCC7A0]/80 backdrop-blur-sm border-t border-[#c7b288] z-50">
-      <div className="flex justify-around items-center h-20">
-        <NavLink to="/market">
-          <img src={marketIcon} alt="Market" className="w-8 h-8" />
+    <nav className="w-full bg-[#DCC7A0]/80 backdrop-blur-sm border-t border-[#c7b288]">
+      <div className="flex justify-around items-center h-20 text-xs text-center">
+        <NavLink to="/market" className="flex flex-col items-center">
+          <img src={marketIcon} alt="Market" className="w-7 h-7 mb-1" />
+          <span>Рынок</span>
         </NavLink>
-        <NavLink to="/japa">
-          <img src={japaIcon} alt="Japa" className="w-8 h-8" />
+        <NavLink to="/japa" className="flex flex-col items-center">
+          <img src={japaIcon} alt="Japa" className="w-7 h-7 mb-1" />
+          <span>Джапа</span>
         </NavLink>
-        <NavLink to="/altar">
+        <NavLink to="/altar" className="flex flex-col items-center">
           <img
             src={altarIcon}
             alt="Altar"
-            className="w-14 h-14 rounded-full border-2 border-yellow-500 -mt-4 bg-white"
+            className="w-12 h-12 mb-0 -mt-4 rounded-full border-2 border-yellow-500 bg-white"
           />
+          <span className="-mt-1">Алтарь</span>
         </NavLink>
-        <NavLink to="/gyan">
-          <img src={gyanIcon} alt="Gyan" className="w-8 h-8" />
+        <NavLink to="/gyan" className="flex flex-col items-center">
+          <img src={gyanIcon} alt="Gyan" className="w-7 h-7 mb-1" />
+          <span>Гьяна</span>
         </NavLink>
-        <NavLink to="/shiksha">
-          <img src={shikshaIcon} alt="Shiksha" className="w-8 h-8" />
+        <NavLink to="/shiksha" className="flex flex-col items-center">
+          <img src={shikshaIcon} alt="Shiksha" className="w-7 h-7 mb-1" />
+          <span>Шикша</span>
         </NavLink>
       </div>
     </nav>
