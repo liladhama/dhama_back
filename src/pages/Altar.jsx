f4d8f3a73ee6f7cc70ffd90023cc2aeea392d624
liladhama <samadhidas108@gmail.com>
2025-05-24 10:53:15 +0400

    fix: restore altar size and prevent nav push

--- src/pages/Altar.jsx
+++ src/pages/Altar.jsx
@@ -2,20 +2,15 @@ import React from 'react';
 
 export default function Altar() {
   return (
-    <div className="flex flex-col flex-grow min-h-screen">
-      <div className="relative flex-grow overflow-hidden">
-        {/* Фоновое изображение */}
-        <img
-          src="/images/altar-bg.png"
-          alt="Altar"
-          className="absolute inset-0 w-full h-full object-cover z-0"
-        />
-
-        {/* Под интерактивные элементы */}
-        <div className="relative z-10 h-full w-full flex items-end justify-center pb-4">
-          {/* Подношения будут здесь */}
-        </div>
-      </div>
+    <div
+      className="bg-cover bg-center bg-no-repeat"
+      style={{
+        backgroundImage: "url('/images/altar-bg.png')",
+        height: 'calc(100vh - 136px)', // TopBar + BottomNav
+        overflow: 'hidden'
+      }}
+    >
+      {/* Можно добавить кнопки подношений здесь */}
     </div>
   );
 }
