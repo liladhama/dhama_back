16f8dd5397c21211ff3bcce1b00f3abb473ad680
liladhama <samadhidas108@gmail.com>
2025-05-24 23:48:24 +0400

    Fix default route and preload background

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import TopBar from './components/TopBar';
 import BottomNav from './components/BottomNav';
