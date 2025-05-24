644122872016aa5cb862798751ab7ce8371b4e15
liladhama <samadhidas108@gmail.com>
2025-05-24 19:58:51 +0400

    Обновлён app.js и интеграция фона алтаря

--- src/pages/Altar.jsx
+++ src/pages/Altar.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Altar() {
   return (
     <div
-      className="w-full h-full bg-cover bg-center bg-no-repeat"
+      className="bg-cover bg-center bg-no-repeat"
       style={{
         backgroundImage: `url(${process.env.PUBLIC_URL}/images/altar-bg.png)`,
+        height: 'calc(100vh - 136px)',
+        overflow: 'hidden',
       }}
-    >
-      {/* Доп. элементы на алтаре */}
-    </div>
+    />
   );
 }
