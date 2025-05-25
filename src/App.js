940466f06f7c84ea0070aa229bfadf1fe3dbe24b
liladhama <samadhidas108@gmail.com>
2025-05-25 20:59:12 +0400

    Build: финальная версия App.jsx с видеофоном .webm

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import TopBar from './components/TopBar';
+import BottomNav from './components/BottomNav';
+
+import Altar from './pages/Altar';
+import Japa from './pages/Japa';
+import Gyan from './pages/Gyan';
+import Shiksha from './pages/Shiksha';
+import Market from './pages/Market';
+
 export default function App() {
   return (
-    <div className="relative min-h-[100dvh] bg-black">
+    <div className="relative flex flex-col min-h-[100dvh] overflow-hidden">
+      {/* Фоновое видео */}
       <video
         autoPlay
         loop
         muted
         playsInline
-        className="fixed top-0 left-0 w-full h-full object-cover"
+        className="fixed top-0 left-0 w-full h-full object-cover -z-10"
       >
         <source src="/videos/hanuman-intro.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
 
-      <div className="relative z-10 text-white text-center p-10">
-        <h1>Тест видеофона</h1>
+      {/* Интерфейс */}
+      <TopBar />
+      <div className="flex-grow relative z-10 overflow-y-auto">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Altar />} />
+            <Route path="/japa" element={<Japa />} />
+            <Route path="/gyan" element={<Gyan />} />
+            <Route path="/shiksha" element={<Shiksha />} />
+            <Route path="/market" element={<Market />} />
+          </Routes>
+        </BrowserRouter>
       </div>
+      <BottomNav />
     </div>
   );
 }
