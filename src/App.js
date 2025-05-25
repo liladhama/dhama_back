401876945d4641fc67e2d050d0661f90e64305c5
liladhama <samadhidas108@gmail.com>
2025-05-25 20:56:55 +0400

    Fix: заменено видео на .webm для поддержки мобильных

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
         playsInline
         className="fixed top-0 left-0 w-full h-full object-cover"
       >
-        <source src="/videos/hanuman-intro-light.mp4" type="video/mp4" />
+        <source src="/videos/hanuman-intro.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
 
