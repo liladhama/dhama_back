7cfcb2c2c28ed0c5660b98fe22a66e18ffb38194
liladhama <samadhidas108@gmail.com>
2025-05-26 14:33:40 +0400

    Fix: скрытие TopBar и BottomNav на Intro в App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 
-import Intro from './pages/Intro'; // ← добавили
+import Intro from './pages/Intro';
 import Altar from './pages/Altar';
 import Japa from './pages/Japa';
 import Gyan from './pages/Gyan';
@@ -11,23 +11,32 @@ import Market from './pages/Market';
 import TopBar from './components/TopBar';
 import BottomNav from './components/BottomNav';
 
+function LayoutWrapper() {
+  const location = useLocation();
+  const isIntro = location.pathname === '/';
+
+  return (
+    <div className="flex flex-col h-screen overflow-hidden">
+      {!isIntro && <TopBar />}
+      <div className="flex-grow overflow-hidden">
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/altar" element={<Altar />} />
+          <Route path="/japa" element={<Japa />} />
+          <Route path="/gyan" element={<Gyan />} />
+          <Route path="/shiksha" element={<Shiksha />} />
+          <Route path="/market" element={<Market />} />
+        </Routes>
+      </div>
+      {!isIntro && <BottomNav />}
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="flex flex-col h-screen overflow-hidden">
-        <TopBar />
-        <div className="flex-grow overflow-hidden">
-          <Routes>
-            <Route path="/" element={<Intro />} />
-            <Route path="/altar" element={<Altar />} />
-            <Route path="/japa" element={<Japa />} />
-            <Route path="/gyan" element={<Gyan />} />
-            <Route path="/shiksha" element={<Shiksha />} />
-            <Route path="/market" element={<Market />} />
-          </Routes>
-        </div>
-        <BottomNav />
-      </div>
+      <LayoutWrapper />
     </BrowserRouter>
   );
 }
